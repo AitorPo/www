3fd5a62074c509bf004edb4a61c87d5ec3fb76e2
AitorPo <aitorpoquetgin@gmail.com>
2020-11-05 11:15:52 +0100

    PDO con gestión de errores en el registro

--- Actividad_Aprendizaje_Aitor_Poquet_1_Eval/signup.php
+++ Actividad_Aprendizaje_Aitor_Poquet_1_Eval/signup.php
@@ -105,7 +105,7 @@ if(isset($_POST)){
                  send_Mail($email, $name, $from, $subject, $body);
             }
                         
-            //Gestion de errores de los campos del form
+            //Gestion de errores de los campos del formulario
         }catch(PDOException $e){
             if (isset($email)) {
                 $_SESSION['errors']['global'] = 'El email ya existe. Prueba con otro';
